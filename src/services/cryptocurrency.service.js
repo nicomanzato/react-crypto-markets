026e42909ecf269772bdc2390e8ed3551b3efbb4
Nicolas Manzato <nicolasmanzato@MacBook-Pro-de-Nicolas.local>
2019-09-08 20:40:25 -0300

    fixed service endpoint

--- src/services/cryptocurrency.service.js
+++ src/services/cryptocurrency.service.js
@@ -7,7 +7,7 @@ import CryptocurrencyMock from './mock/cryptocurrency.mock.json';
 export const getCryptocurrencies = async () => {
   let data;
 
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV !== 'development') {
     data = (await Axios.get(process.env.REACT_APP_CRYPTOCURRENCY_API + constants.ENDPOINTS.GET_CRYPTOCURRENCIES)).data;
   } else {
     data = CryptocurrencyMock;
