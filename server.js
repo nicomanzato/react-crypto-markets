780e7b6f997ce9fdf05568441d721609ae38d8ba
Nicolas Manzato <nicolasmanzato@MacBook-Pro-de-Nicolas.local>
2019-09-08 18:34:43 -0300

    express server.js updated

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const port = process.env.PORT || 8080;
 const app = express();
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build/assets/svg')));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function(req, res) {
   return res.send('pong');
