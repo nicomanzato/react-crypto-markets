e337cced1d442ca7a96e712b4c74a44e3ce5a76e
Nicolas Manzato <nicolasmanzato@MacBook-Pro-de-Nicolas.local>
2019-09-08 18:09:16 -0300

    adding asset to express access

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'assets')));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function(req, res) {
   return res.send('pong');
