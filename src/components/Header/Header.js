874df6dd2c0be7db7ad239ab9041e9e669c1830f
Nicolas Manzato <nicolasmanzato@MacBook-Pro-de-Nicolas.local>
2019-09-14 17:18:08 -0300

    header update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,9 @@ export const Header = ({ className, data }) => {
   return (
     <div className="header">
       <div className="header__title">Crypto Markets</div>
+      <div className="header__nav">
+        <span href="#" className="header__nav-element"></span>
+      </div>
     </div>
   );
 };
