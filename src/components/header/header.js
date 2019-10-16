50d84cb49c14487e2542eb826259339797c20018
Nicolas Manzato <nicomanzato@gmail.com>
2019-10-16 20:41:54 -0300

    case sensitive file names updated

---
+++ src/components/header/header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './header.scss';
+
+import { withRouter } from 'react-router-dom';
+
+// CONSTANTS
+import { constants } from '../../constants/constants';
+
+export const Header = ({ className, data, history }) => {
+  const handleOnTitleClick = () => {
+    history.push(constants.ROUTES.CRYPTOCURRENCY_MARKETS);
+  };
+  return (
+    <div className="header">
+      <div onClick={handleOnTitleClick} className="header__title">
+        Crypto Markets
+      </div>
+      <div className="header__nav">
+        <span href="#" className="header__nav-element"></span>
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(Header);
---
+++ src/pages/markets/markets.page.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './markets.page.scss';
+
+// REDUX
+import { connect } from 'react-redux';
+
+// COMPONENTS
+import { CryptocurrencyList } from '../../components/cryptocurrency-list/cryptocurrency-list';
+import { LoadingIndicator } from '../../components/common/loading-indicator/loading-indicator';
+
+// CONSTANTS
+import { constants } from '../../constants/constants';
+
+export const MarketsPage = ({ cryptocurrencies, isLoadingCryptocurrencies, LoadCryptocurrencyDetails, history }) => {
+  const handleOnCryptocurrencyClick = cryptocurrencyId => {
+    history.push(constants.ROUTES.CRYPTOCURRENCY_DETAIL.replace(':cryptocurrencyId', cryptocurrencyId));
+  };
+
+  return (
+    <>
+      {!isLoadingCryptocurrencies && (
+        <div className="page markets-page">
+          <CryptocurrencyList data={cryptocurrencies} onElementClick={handleOnCryptocurrencyClick} />
+        </div>
+      )}
+      {isLoadingCryptocurrencies && <LoadingIndicator />}
+    </>
+  );
+};
+
+function mapStateToProps(state, props) {
+  return {
+    cryptocurrencies: state.CryptocurrencyReducer.cryptocurrencies,
+    isLoadingCryptocurrencies: state.CryptocurrencyReducer.isLoadingCryptocurrencies,
+  };
+}
+
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MarketsPage);
